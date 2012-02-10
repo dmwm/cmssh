62103ee9032987d97964a9f52b127f1d8577e874
Valentin Kuznetsov <vkuznet@gmail.com>
2012-02-10 11:19:57 -0500

    New tag

--- cmssh_install.py
+++ cmssh_install.py
@@ -320,7 +320,7 @@ def main():
     except:
         pass
 #    url = 'http://github.com/vkuznet/cmssh/tarball/master/'
-    url = 'http://github.com/vkuznet/cmssh/tarball/v0.3/'
+    url = 'http://github.com/vkuznet/cmssh/tarball/v0.4/'
     get_file(url, 'cmssh.tar.gz', path, debug, check=False)
     cmd = 'mv vkuznet-cmssh* %s/cmssh' % path
     exe_cmd(path, cmd, debug)
