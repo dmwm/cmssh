9bdbd58eaa5fbc3328e8b604779e804b44e6af35
Valentin Kuznetsov <vkuznet@gmail.com>
2012-04-06 13:27:14 -0400

    Add __len__, __getitem__ operators

--- src/cmssh/results.py
+++ src/cmssh/results.py
@@ -5,6 +5,8 @@
 ResultManager holds current data returned by cms-sh
 """
 
+import types
+
 class ResultManager(object):
     """This class holds results of every command used in cms-sh"""
     def __init__(self, debug=0):
@@ -24,3 +26,25 @@ class ResultManager(object):
     def __iter__(self):
         """local iterator"""
         return iter(self.data)
+
+    def __len__(self):
+        """len operator"""
+        if  isinstance(self.data, types.GeneratorType):
+            return len([i for i in self.data])
+        elif isinstance(self.data, list):
+            return len(self.data)
+        else:
+             raise TypeError
+
+    def __getitem__(self, idx):
+        """getitem operator"""
+        if  isinstance(self.data, types.GeneratorType):
+            if  not idx:
+                return self.data.next()
+            for _ in range(0, idx-1):
+                self.data.next()
+            return self.data.next()
+        elif isinstance(self.data, list):
+            return self.data.__getitem__(idx)
+        else:
+             raise TypeError
