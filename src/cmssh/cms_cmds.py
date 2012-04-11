2e454e27df86bf9d2f9266c9a975a69c82dd28cd
Valentin Kuznetsov <vkuznet@gmail.com>
2012-04-11 10:55:07 -0400

    Use TagCollector to get list of (non)active releases

--- src/cmssh/cms_cmds.py
+++ src/cmssh/cms_cmds.py
@@ -17,7 +17,8 @@ from cmssh.filemover import copy_lfn, rm_lfn, mkdir, rmdir, list_se, dqueue
 from cmssh.utils import list_results
 from cmssh.cmsfs import dataset_info, block_info, file_info, site_info, run_info
 from cmssh.cmsfs import CMSFS, apply_filter, validate_dbs_instance
-from cmssh.cms_urls import dbs_instances
+from cmssh.cms_urls import dbs_instances, tc_url
+from cmssh.url_utils import get_data
 from cmssh.results import ResultManager
 
 # global scope
@@ -69,7 +70,39 @@ class Magic(object):
 
 def cms_releases(_arg):
     """List available CMS releases"""
-    print "\nAvailable CMS releases:"
+    arch = None
+    platform = os.uname()[0]
+    if  platform == 'Darwin':
+        arch = 'osx'
+    elif platform == 'Linux':
+        arch = 'slc'
+    else:
+        raise Exception('Unsupported platform %s' % os.uname())
+
+    active_releases = get_data(tc_url(), 'py_getActiveReleases')
+    active = {}
+    non_active = {}
+    for rel in active_releases:
+        args = {'release': rel}
+        releases = get_data(tc_url(), 'py_getReleaseArchitectures', args)
+        for item in releases:
+            rel_arch = item[0]
+            status   = item[1]
+            if  rel_arch.find(arch) != -1:
+                if  status:
+                    active.setdefault(rel_arch, []).append(rel)
+                else:
+                    non_active.setdefault(rel_arch, []).append(rel)
+    if  active:
+        print "Supported releases for %s:" % platform.replace('Darwin', 'Mac OS X')
+        for arch, releases in active.items():
+            print '%s: %s' % (arch, ', '.join(releases))
+    if  non_active:
+        print "Un-supported releases for %s:" % platform.replace('Darwin', 'Mac OS X')
+        for arch, releases in non_active.items():
+            print '%s: %s' % (arch, ', '.join(releases))
+
+    print "\nAvailable CMS releases for %s:" % os.environ['SCRAM_ARCH']
     cmd  = "apt-cache search CMSSW | grep CMSSW | grep -v -i fwlite"
     cmd += "| awk '{print $1}' | sed -e 's/cms+cmssw+//g' -e 's/cms+cmssw-patch+//g'"
     subprocess.call(cmd, shell=True)
