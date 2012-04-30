26777918a370bd6f98329170b259557d19f79d79
Valentin Kuznetsov <vkuznet@gmail.com>
2012-04-30 11:41:21 -0400

    First draft

---
+++ src/cmssh/regex.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+"""
+File       : regex.py
+Author     : Valentin Kuznetsov <vkuznet@gmail.com>
+Description: Various regex patterns used by cmssh
+"""
+
+# system modules
+import re
+
+pat_release = re.compile(\
+r'^(release=)?CMSSW(_[0-9]){3}$|(release=)?^CMSSW(_[0-9]){3}_patch[0-9]+$|^(release=)?CMSSW(_[0-9]){3}_pre[0-9]+$')
+pat_lfn = re.compile('.*\.root$')
+pat_site = re.compile('^(site=)?T[0-9]_[A-Z]+(_)[A-Z]+')
+pat_dataset = re.compile('^(dataset=)?/.*/.*/.*')
+pat_block = re.compile('^(block=)?/.*/.*/.*#.*')
+pat_lfn = re.compile('^(file=)?/.*\.root$')
+pat_run = re.compile('^(run=)?[1-9][0-9]{5,8}$')
+pat_se = re.compile('^(site=)?T[0-3]_.*:/.*')
---
+++ src/cmssh/tagcollector.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+"""
+CMS Tag Collector interface
+"""
+
+# system modules
+import re
+
+# cmssh modules
+from cmssh.cms_urls import tc_url
+from cmssh.url_utils import get_data
+from cmssh.regex import pat_release
+
+def releases(rel_name=None):
+    "Return information about CMS releases"
+    if  rel_name:
+        if  not pat_release.match(rel_name):
+            msg = 'Wrong CMSSW release name'
+            raise ValueError(msg)
+        args  = {'release_name': rel_name}
+    else:
+        args  = {}
+    rel_info  = get_data(tc_url(), 'getReleasesInformation', args)
+    columns   = rel_info['columns']
+    pat = re.compile('CMSSW_[1-9]_[0-9]_X\.*')
+    for key, val in rel_info['data'].iteritems():
+        if  pat.match(key):
+            continue
+        row   = {}
+        pairs = zip(columns['release_name'], val)
+        for kkk, vvv in pairs:
+            if  isinstance(kkk, basestring):
+                row[kkk] = vvv
+            elif isinstance(kkk, list):
+                for item in vvv:
+                    row.setdefault('architectures', []).append(dict(zip(kkk, item)))
+        row['release_name'] = key
+        yield row
+
+if __name__ == '__main__':
+    for r in releases():
+        print r
