ce865ec3dcf571431085a1d806f563103c522eae
Valentin Kuznetsov <vkuznet@gmail.com>
2012-05-10 09:19:45 -0400

    Add root_init

--- cmssh_install.py
+++ cmssh_install.py
@@ -585,10 +585,11 @@ python setup.py install --prefix=$idir
         msg += 'png_init=`find $VO_CMS_SW_DIR/$SCRAM_ARCH/external/libpng -name init.sh | tail -1`\n'
         msg += 'lapack_init=`find $VO_CMS_SW_DIR/$SCRAM_ARCH/external/lapack -name init.sh | tail -1`\n'
         msg += 'numpy_init=`find $VO_CMS_SW_DIR/$SCRAM_ARCH/external/py2-numpy -name init.sh | tail -1`\n'
+        msg += 'root_init=`find $VO_CMS_SW_DIR/$SCRAM_ARCH/lcg/root -name init.sh | tail -1`\n'
         msg += 'matplotlib_init=`find $VO_CMS_SW_DIR/$SCRAM_ARCH/external/py2-matplotlib -name init.sh | tail -1`\n'
         msg += 'export PATH=/usr/bin:/bin:/usr/sbin:/sbin\n'
         msg += 'unset PYTHONPATH\n'
-        msg += 'source $apt_init;\n'
+        msg += 'source $apt_init;source $root_init;\n'
         msg += 'source %s\n' % cms_python_env.replace(sdir, '$CMSSH_ROOT/CMSSW').replace(arch, '$SCRAM_ARCH')
         msg += 'source $xz_init;source $pcre_init;\n'
         msg += 'source $matplotlib_init;source $numpy_init;source $lapack_init;source $png_init\n'
